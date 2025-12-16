695b6e34549e0636297210d5b97d569a795b1323
Vedanth <24ug1byai124@bmsit.in>
2025-12-16 15:18:59 +0530

    final in main

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
- well i tried making changes is it ok and added feature now
+ well i tried making changes is it ok and added feature now and now added new file and reverted smtg too
 
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 int main(){
 printf("Hello world this is git and yeaaa i am trying new things heheh");
 printf("This was added from feature branch");
+printf("creating new file");
 return 0;
 }
